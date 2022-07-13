f97b469603c9a4e8fb7fbb5faf6de3b620a97c33
Naughty Squire <naughtysquiredev@icloud.com>
2022-07-13 18:56:50 +0300

    completed "Run Loop, таймеры"

--- Netology_IOS_graduate_work/Controllers/FeedViewController.swift
+++ Netology_IOS_graduate_work/Controllers/FeedViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+
+    private var updateCounter = 15
     private lazy var toPostButton1 =  createToPostButton()
     private lazy var toPostButton2 = createToPostButton()
     private func createToPostButton() -> UIButton{
@@ -17,24 +19,44 @@ class FeedViewController: UIViewController {
         button.addTarget(self, action: #selector(toPostButtonTapHandler), for: .touchUpInside)
         return button
     }
-    private lazy var stackButtons: UIStackView = {
+    private lazy var updateTimerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "До следующего обновления новостей \(self.updateCounter) секунд\(self.getWordEnding(self.updateCounter))"
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.addArrangedSubview(toPostButton1)
         stackView.addArrangedSubview(toPostButton2)
+        stackView.addArrangedSubview(updateTimerLabel)
         stackView.axis = .vertical
         stackView.spacing = 10
         return stackView
     }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){_ in
+            self.updateCounter -= 1
+            if self.updateCounter <= 0 {
+                self.updateCounter = 15
+                self.updateFeed()
+            }
+            self.updateTimerLabel.text = "До следующего обновления новостей \(self.updateCounter) секунд\(self.getWordEnding(self.updateCounter))"
+
+        }
+        timer.tolerance = 0.2
         self.view.backgroundColor = .systemBackground
         self.title = "Feed"
-        self.view.addSubview(stackButtons)
-        stackButtons.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stackButtons.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackButtons.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
+
     }
 
     @objc
@@ -43,5 +65,18 @@ class FeedViewController: UIViewController {
         let postVC = PostViewController()
         postVC.post = post
         navigationController?.pushViewController(postVC, animated: true)
+    }
+
+    func updateFeed() {}
+
+    func getWordEnding(_ num: Int) -> String{
+        if !(10...20 ~= num % 100){
+        	if num % 10 == 1 {
+        	    return "а"
+        	}else if 2...4 ~= num % 10{
+        	    return "ы"
+        	}
+        }
+        return ""
     }
 }
